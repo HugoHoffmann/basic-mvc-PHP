7f2ed9571d71881c0ee9946a9a9792d08566d8fb
“Hugo <hugohoffmann04@gmail.com>
2018-11-22 20:48:47 -0200

    Add ModelCliente.php

---
+++ ModelCliente.php
@@ -0,0 +1,21 @@
+<?php
+class ModelCliente{
+    private $codigo;
+    private $nome;
+    
+    public function getCodigo() {
+        return $this->codigo;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function setCodigo($codigo) {
+        $this->codigo = $codigo;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+}
